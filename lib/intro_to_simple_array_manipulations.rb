f0c004525b44b0fd70ab45cf4acc914a0615a26f
Abraham Moreno <adiaz2821@gmail.com>
2019-09-17 12:51:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,19 +5,22 @@ colors_in_the_rainbow.unshift
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 colors_in_the_rainbow.pop
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
+colors_in_the_rainbow.pop("orange")
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+colors_in_the_rainbow.shift
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+colors_in_the_rainbow.shift()
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+colors_in_the_rainbow.concat
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+colors_in_the_rainbow.insert
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+colors_in_the_rainbow.uniq
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
+colors_in_the_rainbow.flatten
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
+colors_in_the_rainbow.delete
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
-colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
-colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
-colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
-colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+colors_in_the_rainbow.delete
