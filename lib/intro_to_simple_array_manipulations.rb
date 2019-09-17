d71ca3f56ccb19cc00e135cd4aee73efdd1c3ab0
Abraham Moreno <adiaz2821@gmail.com>
2019-09-17 12:43:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,23 @@
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+colors_in_the_rainbow.push =
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
