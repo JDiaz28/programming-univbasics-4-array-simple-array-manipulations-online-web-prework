55066b73bda4f1c32735d2cd61d5474a0b428fa6
Abraham Moreno <adiaz2821@gmail.com>
2019-09-17 12:46:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,9 @@
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 colors_in_the_rainbow.push("violet")
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
+colors_in_the_rainbow.unshift
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-
+colors_in_the_rainbow.pop
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
